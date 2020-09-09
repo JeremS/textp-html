f03024e23e9db86e2c38572d4b9c34d0e327628d
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-09-10 00:03:54 +0200

    Reworked the build process.

---
+++ src/build/fr/jeremyschoffen/textp/alpha/html/docs/core.clj
@@ -0,0 +1,24 @@
+(ns fr.jeremyschoffen.textp.alpha.html.docs.core
+  (:require
+    [fr.jeremyschoffen.textp.alpha.doc.core :as doc]
+    [fr.jeremyschoffen.mbt.alpha.utils :as u]))
+
+(u/pseudo-nss
+  project)
+
+
+(def readme-src "fr/jeremyschoffen/textp/alpha/html/docs/readme/README.md.tp")
+
+(defn make-readme! [{wd ::project/working-dir
+                     maven-coords ::project/maven-coords
+                     git-coords ::project/git-coords}]
+  (spit (u/safer-path wd "README.md")
+        (doc/make-document readme-src
+                           {:project/maven-coords maven-coords
+                            :project/git-coords git-coords})))
+
+
+(comment
+  (-> readme-src
+      doc/slurp-resource
+      doc/read-document))
